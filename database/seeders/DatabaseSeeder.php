b9be9477a32393b502c6e03b13737cecc9df3113
IanMarcello <ian.marcello.jada19@gmail.com>
2022-12-14 00:20:58 +0300

    Updated db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\App;
+use App\Models\Order;
+use App\Models\Transaction;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +18,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(10)->create();
+        App::factory(10)->create();
+        Order::factory(10)->create();
+        Transaction::factory(10)->create();
     }
 }
