7f68c1368536417d521d1d3ceedc28fe8d655e02
IanMarcello <ian.marcello.jada19@gmail.com>
2022-12-18 22:35:14 +0300

    Updated routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/v1/orders', [OrderController::class, 'index'])->name('api.orders.index');
+Route::post('/v1/orders/create', [OrderController::class, 'create'])->name('api.orders.create');
+Route::get('/v1/order/{orderId}', [OrderController::class, 'show'])->name('api.orders.show');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Openpesa\Pesa\Facades\Pesa;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('api.orders.show');
+});
+
+Route::get('/test-api', function () {
+    $data = [
+        'firstName' => 'Levina',
+        'lastName' => 'Pamba',
+        'amount' => 100000,
+        'phone_number' => '255762897200'
+    ];
+    $response = Pesa::c2b([
+        'input_Amount' => $data['amount'], // Amount to be charged
+        'input_Country' => 'TZN',
+        'input_Currency' => 'TZS',
+        'input_CustomerMSISDN' => $data['phone_number'], // replace with your phone number
+        'input_ServiceProviderCode' => '000000', // replace with your service provider code given by M-Pesa
+        'input_ThirdPartyConversationID' => 'rasderekf', // unique
+        'input_TransactionReference' => 'asdodfdferre', // unique
+        'input_PurchasedItemsDesc' => 'Test Item',
+    ]);
+    dd($response);
 });
