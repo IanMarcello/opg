51683e0432ff10b29d65ac8e8d3b9e6d7c5268bb
IanMarcello <ian.marcello.jada19@gmail.com>
2023-01-01 06:34:48 +0300

    Update UserTable.php

--- app/Http/Livewire/UserTable.php
+++ app/Http/Livewire/UserTable.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\Views\Columns\BooleanColumn;
 
 class UserTable extends DataTableComponent
 {
@@ -12,19 +14,27 @@ class UserTable extends DataTableComponent
 
     public function configure(): void
     {
-        $this->setPrimaryKey('id');
+        $this->setPrimaryKey('id')
+            ->setHideBulkActionsWhenEmptyEnabled();
     }
 
     public function columns(): array
     {
         return [
-            Column::make("Id", "id")
-                ->sortable(),
+            Column::make("Role", "role")
+                ->sortable()
+                ->excludeFromColumnSelect(),
             Column::make("Name", "name")
-                ->sortable(),
+                ->sortable()
+                ->searchable()
+                ->excludeFromColumnSelect(),
             Column::make("Email", "email")
-                ->sortable(),
+                ->sortable()
+                ->searchable(),
             Column::make("Phone number", "phone_number")
+                ->sortable()
+                ->searchable(),
+            BooleanColumn::make("Status", "status")
                 ->sortable(),
             Column::make("Created at", "created_at")
                 ->sortable(),
@@ -32,4 +42,25 @@ class UserTable extends DataTableComponent
                 ->sortable(),
         ];
     }
+    
+    public function builder(): Builder
+    {
+        return User::query()->where('role', 'Client');
+    }
+    
+    public function bulkActions(): array
+    {
+        return [
+            'active' => 'Activate',
+            'inactive' => 'Deactivate'
+        ];
+    }
+    /*
+    public function active() {
+        return true;
+    }
+    
+    public function inactive() {
+        return true;
+    }*/
 }
